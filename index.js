ac0471bd783da8030ab9ff8dab56c2dc60ec8d87
XKlaymoreX <hoza.cristian01@gmail.com>
2020-09-22 16:17:21 +0200

    build and remove client

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.route('/')
     .get((req,res) => {
-        res.sendFile('./client/public/index.html')
+        res.sendFile('index.html', {root: 'client/public'})
     })
 
 app.listen(3000 || process.env.PORT, () => console.log("Listening on port..."))
