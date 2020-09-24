a55289e202b84c0dd2274efa2ac9fa176c0fbbfe
unknown <hoza.cristian01@gmail.com>
2020-09-24 16:02:48 +0200

    editProcfile

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const path = require('path')
 const app = express()
 const PORT = 5000 || process.env.PORT;
 
-
-
+console.log("MUXONE")
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
     console.log("Muxone")
@@ -12,7 +11,5 @@ if(process.env.NODE_ENV === "production"){
         res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
 }
-
-
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
