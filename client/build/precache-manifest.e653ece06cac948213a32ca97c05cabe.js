b047360a4de13b7e6dc51b60051eb87f84f015a8
unknown <hoza.cristian01@gmail.com>
2020-09-24 16:46:27 +0200

    addedClient

---
+++ client/build/precache-manifest.e653ece06cac948213a32ca97c05cabe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a60ccb342bae6d727455454aa7662e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2638f6ebf903eeab4dd",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "13d5a79fb5228e358044",
+    "url": "/static/css/main.8c41bc56.chunk.css"
+  },
+  {
+    "revision": "f2638f6ebf903eeab4dd",
+    "url": "/static/js/2.b2512745.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2512745.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d5a79fb5228e358044",
+    "url": "/static/js/main.28948980.chunk.js"
+  },
+  {
+    "revision": "9413ce73a456bed8b559",
+    "url": "/static/js/runtime-main.535267eb.js"
+  },
+  {
+    "revision": "1d89ac50857fa243df48dbf3d97756f8",
+    "url": "/static/media/wedding.1d89ac50.jpg"
+  }
+]);
