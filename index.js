2438f26234b99f71776cf7a5ba46ec833ef971d3
unknown <hoza.cristian01@gmail.com>
2020-09-24 15:20:34 +0200

    something

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ const PORT = 5000 || process.env.PORT
 
 app.use("/",express.static(path.join(__dirname,"client","build")))
 
+
+app.get("/",express.static(path.join(__dirname,"client","build")))
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
