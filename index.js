94cc2452dc7d820346d497bda76a27105af57af6
unknown <hoza.cristian01@gmail.com>
2020-09-24 15:45:50 +0200

    process

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT = 5000 || process.env.PORT
+const PORT = 5000 || process.env.PORT;
 
 
-app.use(express.static(path.join(__dirname,"client","build")))
 
-
-app.route("/")
-    .get((req, res) => {
-        res.sendFile("index.html")
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"))
+    console.log("Muxone")
+    app.get("*",(req,res) => {
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
+}
+
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
