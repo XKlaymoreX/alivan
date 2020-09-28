d12b6ec354e1366e70c0a62ebc6d2b2a967859c5
unknown <hoza.cristian01@gmail.com>
2020-09-28 12:27:37 +0200

    responsiveInfoSection

--- client/src/components/InfoSection.tsx
+++ client/src/components/InfoSection.tsx
@@ -7,11 +7,10 @@ import RestaurantSVG from '../media/svgs/restaurant.svg'
 
 const InfoSection : React.FC = () => {
     return (
-        <div className="d-flex flex-row justify-content-around">
+        <div className="infoGrid">
             <InfoCard svg={CalendarSVG} info="01/07/2021" details="1 Luglio 2021" href={""} hasLink={false} />
             <InfoCard svg={MapSVG} info="Monforte d'Alba" details="Vai alla Mappa" href={"https://goo.gl/maps/5yWbVswnEs46aqeA9"} hasLink={true}/>
             <InfoCard svg={RestaurantSVG} info="Ristorante" details="La Torricella" href={""} hasLink={false} />
-
         </div>
     )
 }
