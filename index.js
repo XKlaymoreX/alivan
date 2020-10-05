0a7dbbf5155c4b76ba2c14f9c500e79fc8e3e1c1
hoza.cristian01 <hoza.cristian01@gmail.com>
2020-10-05 10:02:28 +0200

    muxone

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3306;
-const dotenv = require('dotenv')
-dotenv.config({ path: './.env' })
+
 
 app.use(express.static("client/build"))
 
