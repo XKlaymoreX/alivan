1dd14a52d78dbd0449bba57bac6834fb9d587194
hoza.cristian01 <hoza.cristian01@gmail.com>
2020-10-05 10:06:21 +0200

    new route disposition

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ app.use(express.static("client/build"))
 
 if (process.env.NODE_ENV == "production") {
     app.use('/Private', require('./routes/private'))
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 } else {
-    const dotenv = require('dotenv')
-    dotenv.config({ path: './.env' })
     app.use('/Private', require('./routes/private'))
     app.get("/*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
