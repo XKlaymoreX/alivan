8c7d511dc41df2c3202500aa083c6e3c16283bb0
hoza.cristian01 <hoza.cristian01@gmail.com>
2020-10-05 10:00:27 +0200

    dotenv fix

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const PORT =  process.env.PORT || 3306;
+const PORT = process.env.PORT || 3306;
 const dotenv = require('dotenv')
-dotenv.config({path:'./.env'})
+dotenv.config({ path: './.env' })
 
 app.use(express.static("client/build"))
 
-if(process.env.NODE_ENV == "production"){
+if (process.env.NODE_ENV == "production") {
     app.use('/Private', require('./routes/private'))
-    app.get("/*",(req,res) => {
-        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    app.get("/*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
-}else{
-    app.use("/", require('./routes/private'))
+} else {
+    const dotenv = require('dotenv')
+    dotenv.config({ path: './.env' })
+    app.use('/Private', require('./routes/private'))
+    app.get("/*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
 }
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
